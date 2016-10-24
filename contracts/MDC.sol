37515d6cdd66d73569de53d27c033919552c39ee
yanghb <yanghb_1@163.com>
2016-10-24 17:32:51 +0800

    change verb name

--- contracts/MDC.sol
+++ contracts/MDC.sol
@@ -26,35 +26,35 @@ contract MDC {
     mapping (uint => ClaimInfo) claims;
     uint public totalClaims;
     
-    uint public operating_charge_balance;
+    uint public operatingChargeBalance;
 
     uint public totalBalances;
     address public organizer;
     
-    uint public max_operating_charge;
+    uint public maxOperatingCharge;
     
-    uint8 public recommendation_reward_rate;
-    uint8 public operating_charge_rate;
-    uint public claim_fee;
+    uint8 public recommendationRewardRate;
+    uint8 public operatingChargeRate;
+    uint public claimFee;
 
     uint8 public status; // 0: no claim, 2: has claim
 
     modifier organizerRequired() { if (organizer != msg.sender) throw; _ }
 
-	function MDC(uint _max_operating_charge) {
+	function MDC(uint _maxOperatingCharge) {
         organizer = msg.sender;
-        if(_max_operating_charge == 0){
-            max_operating_charge = 1000;
+        if(_maxOperatingCharge == 0){
+            maxOperatingCharge = 1000;
         }else{
-            max_operating_charge = _max_operating_charge;
+            maxOperatingCharge = _maxOperatingCharge;
         }
 	}
 	
-	function changeSettings(uint8 _recommendation_reward_rate, uint8 _operating_charge_rate, uint _claim_fee) organizerRequired{
-    	if(_recommendation_reward_rate > 100 || _operating_charge_rate > 100 || _recommendation_reward_rate + _operating_charge_rate > 100) throw;
-    	recommendation_reward_rate = _recommendation_reward_rate;
-    	operating_charge_rate = _operating_charge_rate;
-    	claim_fee = _claim_fee;
+	function changeSettings(uint8 _recommendationRewardRate, uint8 _operatingChargeRate, uint _claimFee) organizerRequired{
+    	if(_recommendationRewardRate > 100 || _operatingChargeRate > 100 || _recommendationRewardRate + _operatingChargeRate > 100) throw;
+    	recommendationRewardRate = _recommendationRewardRate;
+    	operatingChargeRate = _operatingChargeRate;
+    	claimFee = _claimFee;
 	}
 
 	function transferOrganizer(address new_organizer) organizerRequired {
@@ -65,9 +65,9 @@ contract MDC {
     	if(msg.value <= 0) throw;
     	uint recommender_fee = 0;
     	if(recommender != address(0)){
-        	recommender_fee = msg.value * recommendation_reward_rate / 100;
+        	recommender_fee = msg.value * recommendationRewardRate / 100;
     	}
-    	uint operating_charge_fee = msg.value * operating_charge_rate / 100;
+    	uint operating_charge_fee = msg.value * operatingChargeRate / 100;
     	uint user_fee = msg.value - recommender_fee - operating_charge_fee;
     	if(user_fee + operating_charge_fee + recommender_fee != msg.value) throw;
     	
@@ -85,13 +85,13 @@ contract MDC {
     	
     	balances[msg.sender] += user_fee;
     	balances[recommender] += recommender_fee;
-    	operating_charge_balance += operating_charge_fee;
+    	operatingChargeBalance += operating_charge_fee;
     	totalBalances += user_fee + operating_charge_fee;
 	}
 	
 	function claim(bytes32 _name, bytes32 _country, bytes32 _id, uint _birthdate, bytes32 _phone, bytes32 _email, uint _timestamp, bytes32 _noncestr, bytes32 _reason) {
-    	if(operating_charge_balance < max_operating_charge) throw;
-    	if(balances[msg.sender] < claim_fee) throw;
+    	if(operatingChargeBalance < maxOperatingCharge) throw;
+    	if(balances[msg.sender] < claimFee) throw;
     	if(status != 0) throw;
     	
         totalClaims++;
@@ -112,33 +112,48 @@ contract MDC {
             status: 0
         });
         
-        balances[msg.sender] -= claim_fee;
-        operating_charge_balance += claim_fee;
+        balances[msg.sender] -= claimFee;
+        operatingChargeBalance += claimFee;
 	}
 	
-	function investigateClaim(uint claimID, uint _need_max_operating_charge) organizerRequired {
-    	if(_need_max_operating_charge > max_operating_charge) throw;
+	function getClaim(uint claimID) returns(address claimer, bytes32 reason, uint8 claimStatus, bytes32 claimerName, bytes32 claimerCountry, bytes32 claimerId, uint claimerBirthdate, bytes32 claimerPhone, bytes32 claimerEmail, uint claimerTimestamp, bytes32 claimerNoncestr) {
+    	ClaimInfo claim = claims[claimID];
+    	claimer = claim.claimer;
+    	reason = claim.reason;
+    	claimStatus = claim.status;
+    	claimerName = claim.userInfo.name;
+    	claimerCountry = claim.userInfo.country;
+    	claimerId = claim.userInfo.id;
+    	claimerBirthdate = claim.userInfo.birthdate;
+    	claimerPhone = claim.userInfo.phone;
+    	claimerEmail = claim.userInfo.email;
+    	claimerTimestamp = claim.userInfo.timestamp;
+    	claimerNoncestr = claim.userInfo.noncestr;
+	}
+	
+	function investigateClaim(uint claimID, uint _needMaxOperatingCharge) organizerRequired {
+    	if(_needMaxOperatingCharge > maxOperatingCharge) throw;
     	ClaimInfo claim = claims[claimID];
     	if(claim.status != 0) throw;
     	
-    	if(operating_charge_balance < _need_max_operating_charge || totalBalances < _need_max_operating_charge) throw;
-    	if(!organizer.send(_need_max_operating_charge)) throw;
-    	operating_charge_balance -= _need_max_operating_charge;
-    	totalBalances -= _need_max_operating_charge;
+    	if(operatingChargeBalance < _needMaxOperatingCharge || totalBalances < _needMaxOperatingCharge) throw;
+    	if(!organizer.send(_needMaxOperatingCharge)) throw;
+    	operatingChargeBalance -= _needMaxOperatingCharge;
+    	totalBalances -= _needMaxOperatingCharge;
     	
     	claim.status = 2;
 	}
 	
-	function passClaim(uint claimID, uint8 compensation_rate, uint8 max_compensation) organizerRequired {
+	function passClaim(uint claimID, uint8 compensationRate, uint8 max_compensation) organizerRequired {
     	ClaimInfo claim = claims[claimID];
     	if(claim.status != 2) throw;
-        if(totalBalances * compensation_rate / 100 > max_compensation) throw;
+        if(totalBalances * compensationRate / 100 > max_compensation) throw;
 
         uint compensation = 0;
         for(uint i=1; i<=totalUserAddresses; i++){
             uint userBalance = balances[userAddresses[totalUserAddresses]];
-            if(userBalance >= claim_fee){
-                uint userCompensation = userBalance * compensation_rate / 100;
+            if(userBalance >= claimFee){
+                uint userCompensation = userBalance * compensationRate / 100;
                 if(userCompensation > 0 && userCompensation <= userBalance){
                     balances[userAddresses[totalUserAddresses]] -= userCompensation;
                     compensation += userCompensation;

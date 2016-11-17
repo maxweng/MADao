1d1d17089128de723c01c7522e062da17b01332c
yanghb <yanghb_1@163.com>
2016-11-17 14:43:45 +0800

    new MDC init

--- contracts/MDC.sol
+++ contracts/MDC.sol
@@ -1,185 +1,224 @@
-contract MDC {
-    struct UserInfo{
-        bytes32 name;
-        bytes32 country;
-        bytes32 id;
-        uint birthdate;
-        bytes32 phone;
-        bytes32 email;
-        uint timestamp;
-        bytes32 noncestr;
-    }
-    
+import "./api/OracleItAPI.sol";
+import "./api/Utils.sol";
+
+contract MDC is usingOracleIt, usingUtils {    
     struct ClaimInfo{
         address claimer;
-        bytes32 reason;
-        UserInfo userInfo;
+        bytes32 claimerName;
+        bytes32 claimerCountry;
+        bytes32 claimerId;
+        bytes32 claimerNoncestr;
+        uint oracleItId;
         uint8 status; // 0: init, 2: investigating, 6: passed, 8: rejected
     }
-
+    
+    struct Flight{
+        bytes32 flightNumber;
+        uint departureTime;
+        bool claimed;
+    }
+    
+    struct UserInfo{
+        bytes32 hash;
+        bool available;
+    }
+    
     mapping (address => uint) public balances;
-    uint public totalBalances;
-
-    mapping (address => bytes32) public infoHashes;
+    
+    mapping (address => UserInfo) public infoHashes;
+    mapping (address => Flight[]) flights;
     
     mapping (uint => address) public userAddresses;
     uint public totalUserAddresses;
     
-    mapping (uint => ClaimInfo) claims;
+    uint public totalAvailableUserAddresses;
+        
+    mapping (uint => ClaimInfo) public claims;
     uint public totalClaims;
     
-    uint public operatingChargeBalance;
-
-    address public organizer;
+    mapping (uint => uint) public oracleItIdClaimId;
     
-    uint public maxOperatingCharge;
-    uint public averageOperatingCharge;
+    uint8 constant recommendationRewardRate = 10;
     
-    uint8 public recommendationRewardRate;
-    uint8 public operatingChargeRate;
-    uint public claimFee;
-
-    uint8 public status; // 0: no claim, 2: has claim
-
-    modifier organizerRequired() { if (organizer != msg.sender) throw; _ }
-
-	function MDC(uint _maxOperatingCharge) {
-        organizer = msg.sender;
-        if(_maxOperatingCharge == 0){
-            maxOperatingCharge = 10 ether;
+    uint constant defaultGasLimit = 1000000;
+    uint constant defaultGasPrice = 20000000000;
+    
+    uint constant minAvailableUsers = 20000;
+    uint constant minUserAvailableBalance = 0.5 ether;
+    uint constant maxCompensation = minAvailableUsers * minUserAvailableBalance;
+    
+    modifier userAvailable() { if(!infoHashes[msg.sender].available) throw; _ }
+    
+    function MDC() {
+    }
+    
+    function signUp(bytes32 _name, bytes32 _country, bytes32 _id, bytes32 _noncestr, address recommender) {
+        if(msg.value < minUserAvailableBalance) throw;
+        uint recommender_fee = 0;
+        if(recommender != address(0)){
+            recommender_fee = msg.value * recommendationRewardRate / 100;
+        }
+        uint user_fee = msg.value - recommender_fee;
+        if(user_fee + recommender_fee != msg.value) throw;
+        
+        bytes32 _infoHash = sha3(_name, _country, _id, _noncestr);
+        
+        if(infoHashes[msg.sender].hash == ""){
+            totalUserAddresses++;
+            totalAvailableUserAddresses++;
+            userAddresses[totalUserAddresses] = msg.sender;
+            infoHashes[msg.sender] = UserInfo({
+                hash: _infoHash,
+                available: true
+            });
         }else{
-            maxOperatingCharge = _maxOperatingCharge;
+            if(infoHashes[msg.sender].hash != _infoHash) throw;
+            if(!infoHashes[msg.sender].available){
+                infoHashes[msg.sender].available = true;
+                totalAvailableUserAddresses++;
+            }
         }
-	}
-	
-	function changeSettings(uint8 _recommendationRewardRate, uint8 _operatingChargeRate, uint _claimFee, uint _averageOperatingCharge) organizerRequired{
-    	if(_averageOperatingCharge > maxOperatingCharge) throw;
-    	if(_recommendationRewardRate > 100 || _operatingChargeRate > 100 || _recommendationRewardRate + _operatingChargeRate > 100) throw;
-    	recommendationRewardRate = _recommendationRewardRate;
-    	operatingChargeRate = _operatingChargeRate;
-    	claimFee = _claimFee;
-    	averageOperatingCharge = _averageOperatingCharge;
-	}
-
-	function transferOrganizer(address new_organizer) organizerRequired {
-    	organizer = new_organizer;
-	}
-
-	function signUp(bytes32 _infoHash, address recommender) {
-    	if(msg.value <= 0) throw;
-    	uint recommender_fee = 0;
-    	if(recommender != address(0)){
-        	recommender_fee = msg.value * recommendationRewardRate / 100;
-    	}
-    	uint operating_charge_fee = msg.value * operatingChargeRate / 100;
-    	uint user_fee = msg.value - recommender_fee - operating_charge_fee;
-    	if(user_fee + operating_charge_fee + recommender_fee != msg.value) throw;
-    	
-    	if(infoHashes[msg.sender] == ""){
-        	totalUserAddresses++;
-        	userAddresses[totalUserAddresses] = msg.sender;
-        	infoHashes[msg.sender] = _infoHash;
-    	}else{
-        	if(infoHashes[msg.sender] != _infoHash) throw;
-    	}
-    	
-    	if(recommender != address(0) && recommender_fee > 0){
-        	if(!recommender.send(recommender_fee)) throw;
+        
+        if(recommender != address(0) && recommender_fee > 0){
+            if(!recommender.send(recommender_fee)) throw;
         }
-    	
-    	balances[msg.sender] += user_fee;
-    	balances[recommender] += recommender_fee;
-    	operatingChargeBalance += operating_charge_fee;
-    	totalBalances += user_fee;
-	}
-	
-	function claim(bytes32 _name, bytes32 _country, bytes32 _id, uint _birthdate, bytes32 _phone, bytes32 _email, uint _timestamp, bytes32 _noncestr, bytes32 _reason) {
-    	if(operatingChargeBalance < averageOperatingCharge) throw;
-    	if(balances[msg.sender] < claimFee) throw;
-    	if(status != 0) throw;
-    	
+        
+        balances[msg.sender] += user_fee;
+        balances[recommender] += recommender_fee;
+    }
+    
+    function minusBalance(address userAddress, uint price) internal {
+        balances[userAddress] -= price;
+        if(balances[userAddress] < minUserAvailableBalance){
+            infoHashes[userAddress].available = false;
+            totalAvailableUserAddresses--;
+        }
+    }
+    
+    function addFlight(bytes32 _flightNumber, uint _departureTime) userAvailable {
+        if(now > _departureTime) throw;
+
+        uint length = flights[msg.sender].length;
+        bool hasFlight = false;
+        for(uint i=0; i<length; i++){
+            var flight = flights[msg.sender][i];
+            if(flight.flightNumber == _flightNumber && flight.departureTime >= _departureTime - 3600 * 2 && flight.departureTime <= _departureTime + 3600 * 2){
+                throw;
+            }
+        }
+        flights[msg.sender].push(Flight({
+            flightNumber: _flightNumber,
+            departureTime: _departureTime,
+            claimed: false
+        }));
+    }
+    
+    function getClaimFee() oracleItAPI internal returns (uint claimFee) {
+        claimFee = oracleIt.getPrice("AirCrash") + defaultGasLimit * defaultGasPrice;
+    }
+    
+    function claim(bytes32 _name, bytes32 _country, bytes32 _id, bytes32 _noncestr, bytes32 _flightNumber, uint _departureTime) userAvailable {
+        if(infoHashes[msg.sender].hash != sha3(_name, _country, _id, _noncestr)) throw;
+
+        uint claimFee = getClaimFee();
+        if(balances[msg.sender] < claimFee) throw;
+        
+        uint length = flights[msg.sender].length;
+        bool hasFlight = false;
+        for(uint i=0; i<length; i++){
+            var flight = flights[msg.sender][i];
+            if(flight.claimed) continue;
+            if(flight.flightNumber == _flightNumber && flight.departureTime == _departureTime){
+                flight.claimed = true;
+                hasFlight = true;
+                break;
+            }
+        }
+        if(!hasFlight) throw;
+        
+        uint oracleItId = oracleItQuery("AirCrash", strConcat(bytes32ToString(_flightNumber), bytes32ToString(uintToBytes(_departureTime))));
+        if(oracleItId == 0) throw;
+        
         totalClaims++;
-        status = 2;
         claims[totalClaims] = ClaimInfo({
             claimer: msg.sender,
-            reason: _reason,
-            userInfo: UserInfo({
-                name: _name,
-                country: _country,
-                id: _id,
-                birthdate: _birthdate,
-                phone: _phone,
-                email: _email,
-                timestamp: _timestamp,
-                noncestr: _noncestr
-            }),
-            status: 0
+
+            claimerName: _name,
+            claimerCountry: _country,
+            claimerId: _id,
+            claimerNoncestr: _noncestr,
+
+            oracleItId: oracleItId,
+            status: 2
         });
         
-        balances[msg.sender] -= claimFee;
-        operatingChargeBalance += claimFee;
-	}
-	
-	function getClaim(uint claimID) returns(address claimer, bytes32 reason, uint8 claimStatus, bytes32 claimerName, bytes32 claimerCountry, bytes32 claimerId, uint claimerBirthdate, bytes32 claimerPhone, bytes32 claimerEmail, uint claimerTimestamp, bytes32 claimerNoncestr) {
-    	ClaimInfo claim = claims[claimID];
-    	claimer = claim.claimer;
-    	reason = claim.reason;
-    	claimStatus = claim.status;
-    	claimerName = claim.userInfo.name;
-    	claimerCountry = claim.userInfo.country;
-    	claimerId = claim.userInfo.id;
-    	claimerBirthdate = claim.userInfo.birthdate;
-    	claimerPhone = claim.userInfo.phone;
-    	claimerEmail = claim.userInfo.email;
-    	claimerTimestamp = claim.userInfo.timestamp;
-    	claimerNoncestr = claim.userInfo.noncestr;
-	}
-	
-	function investigateClaim(uint claimID, uint _needMaxOperatingCharge) organizerRequired {
-    	if(_needMaxOperatingCharge > maxOperatingCharge) throw;
-    	if(_needMaxOperatingCharge > operatingChargeBalance) throw;
-    	ClaimInfo claim = claims[claimID];
-    	if(claim.status != 0) throw;
-    	
-    	if(operatingChargeBalance < _needMaxOperatingCharge) throw;
-    	if(!organizer.send(_needMaxOperatingCharge)) throw;
-    	operatingChargeBalance -= _needMaxOperatingCharge;
-    	
-    	claim.status = 2;
-	}
-	
-	function passClaim(uint claimID, uint8 compensationRate, uint max_compensation) organizerRequired {
-    	ClaimInfo claim = claims[claimID];
-    	if(claim.status != 2) throw;
-        if(totalBalances * compensationRate / 100 > max_compensation) throw;
+        oracleItIdClaimId[oracleItId] = totalClaims;
+        
+        minusBalance(msg.sender, claimFee);
+    }
 
-        uint compensation = 0;
-        for(uint i=1; i<=totalUserAddresses; i++){
-            uint userBalance = balances[userAddresses[totalUserAddresses]];
-            if(userBalance >= claimFee){
-                uint userCompensation = userBalance * compensationRate / 100;
-                if(userCompensation > 0 && userCompensation <= userBalance){
-                    balances[userAddresses[totalUserAddresses]] -= userCompensation;
-                    compensation += userCompensation;
+    function __callback(uint id, string result) {
+        if (msg.sender != oracleItCallbackAddress()) throw;
+        uint claimId = oracleItIdClaimId[id];
+        if (claimId > 0){
+            ClaimInfo claim = claims[claimId];
+            if(claim.status == 2){
+                if(parseInt(result) == 1){
+                    uint compensation = balances[claim.claimer];
+                    minusBalance(claim.claimer, balances[claim.claimer]);
+                    
+                    if(totalAvailableUserAddresses > 0){
+                        uint userCompensation = maxCompensation / totalAvailableUserAddresses;
+                        if(userCompensation > minUserAvailableBalance) userCompensation = minUserAvailableBalance;
+                        for(uint i=1; i<=totalUserAddresses; i++){
+                            address userAddress = userAddresses[i];
+                            if(infoHashes[userAddress].available){
+                                minusBalance(userAddress, userCompensation);
+                                compensation += userCompensation;
+                            }
+                        }
+                    }
+                    
+                    if(!claim.claimer.send(compensation)) throw;
+                    claim.status = 6;
+                }else{
+                    claim.status = 8;
                 }
             }
         }
-
-        if(compensation > totalBalances) throw;
-        if(!claim.claimer.send(compensation)) throw;
-        totalBalances -= compensation;
-
-        claim.status = 6;
-        status = 0;
-	}
-	
-	function rejectClaim(uint claimID) organizerRequired {
-    	ClaimInfo claim = claims[claimID];
-    	if(claim.status != 2) throw;
-    	claim.status = 8;
-    	status = 0;
-	}
-
+    }
+    
+    function uintToBytes(uint v) constant returns (bytes32 ret) {
+        if (v == 0) {
+            ret = '0';
+        }
+        else {
+            while (v > 0) {
+                ret = bytes32(uint(ret) / (2 ** 8));
+                ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+                v /= 10;
+            }
+        }
+        return ret;
+    }
+    
+    function bytes32ToString(bytes32 x) constant returns (string) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
+    
     function () {
         throw;
     }

3ca6a92161d05b6e103a843fb799bdabb5945364
Zhengz <zhengz@masn.cn>
2016-11-30 17:17:33 +0800

    测试微信

--- server/routes.js
+++ server/routes.js
@@ -13,7 +13,7 @@ var routes = {
 
 exports = module.exports = function(app) {
     app.use('/static', express.static(path.join(__dirname, 'static')));
-    
+
     app.use(session({
         secret: settings.secret,
         resave: false, // don't save session if unmodified
@@ -27,45 +27,45 @@ exports = module.exports = function(app) {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
     app.use(middlewares.xml_body_parser);
-    
+
     app.use(middlewares.context_processor);
     app.use(middlewares.set_user);
-    
+
     app.set('port', process.env.PORT || 8000);
-    
+
     app.get('/' + settings.wechatAuthFileName, function(req, res) {
         res.send(settings.wechatAuthFileValue);
     });
-    
+
     app.get('/', function(req, res) {
         res.render("mobile", {});
     });
-    
+
     app.get('/api', function(req, res) {
         res.header("Access-Control-Allow-Origin", "*");
         res.sendStatus(200);
     });
     app.post('/api', routes.api.home);
-    
+
     app.post('/api/login', routes.api.login);
     app.post('/api/logout', routes.api.logout);
     app.all('/api/me', middlewares.login_required, routes.api.me);
     app.post('/api/wechatlogin', middlewares.check_wechat_oauth, routes.api.wechatlogin);
-    
+
     app.all('/api/wechat/mpapi/access_token', routes.api.mpapis.mpapi_access_token_handler);
     app.all('/api/wechat/mpapi/refresh_token', routes.api.mpapis.mpapi_refresh_token_handler);
     app.all('/api/wechat/mpapi/userinfo', routes.api.mpapis.mpapi_userinfo_handler);
-    
+
     if(settings.DEBUG){
         app.get('/wechattest', function(req, res) {
             res.render("wechattest", {});
         });
     }
-    
+
     app.get('/api/coinprice', routes.api.coinprice);
-    
+
     app.all('/api/coinorders', middlewares.login_required, routes.api.coinorders);
     app.post('/api/coinordergetpayparams', middlewares.check_wechat_oauth, middlewares.login_required, routes.api.coinordergetpayparams);
-    
-    app.all('/wxpay/notify', routes.api.wxpaynotify);
+
+    // app.all('/wxpay/notify', routes.api.wxpaynotify);
 };

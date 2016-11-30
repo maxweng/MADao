df2f82200b1366e6ed11110bb7da5a85242070ef
yanghb <yanghb@msan.cn>
2016-11-30 16:07:47 +0800

    promote settings

--- settings.js
+++ settings.js
@@ -22,8 +22,6 @@ settings.avgBlockTime = 15;
 settings.DEFAULT_GAS = 1000000;
 settings.DEFAULT_GAS_PRICE = 20000000000;
 
-settings.VERIFY_BLOCK_NUMBER = 2;
-
 settings.wechatAuthFileName = "MP_verify_KnjeC1ANoqDsJ4kl.txt";
 settings.wechatAuthFileValue = "KnjeC1ANoqDsJ4kl";
 
@@ -53,11 +51,13 @@ if(process.env.NETWORK == "testnet"){
     settings.sysAccountAddress = process.env.SYS_ACCOUNT_ADDRESS || "0x14cB23Cf2deacA1FCCd2F5400c4A8D32595e726f";
     settings.sysAccountPrivateKey = process.env.SYS_ACCOUNT_PRIVATE_KEY || "d74b6bbf373fe501b9483aa29fa65c8c9487ae73d736eea9ad19844f4026f405";
     settings.networkId = process.env.NETWORK_ID || 3;
+    settings.VERIFY_BLOCK_NUMBER = 2;
 }else{
     settings.dbURL = "mongodb://madaodbadmin:Moshi314@123.56.90.147:37017/madao";
     settings.sysAccountAddress = process.env.SYS_ACCOUNT_ADDRESS || "0xb57be5149842f218a95da90599ba2b7a70f888e7";
     settings.sysAccountPrivateKey = process.env.SYS_ACCOUNT_PRIVATE_KEY || "a8d9edff20ef6fd7000f43ec103904bdc173ea55e1008413dcb744d4bf016590";
     settings.networkId = process.env.NETWORK_ID || "default";
+    settings.VERIFY_BLOCK_NUMBER = 0;
 }
 
 settings.networks = {

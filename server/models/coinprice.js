c1cb890a86a23565c98a7708910ce4fdb3d93e56
yanghb <yanghb@msan.cn>
2016-12-21 16:15:54 +0800

    retry getting price

--- server/models/coinprice.js
+++ server/models/coinprice.js
@@ -39,56 +39,66 @@ coinPriceSchema.statics.getPrice = function(tickerId, cb){
 
 coinPriceSchema.statics.refreshAll = function(cb){
     if(typeof(cb) === "undefined") cb = function(){};
-    request({
-        url: "https://yunbi.com/api/v2/tickers",
-        method: "GET"
-    }, function(err, response, body){
-        if(err) return cb(err);
-        if (response.statusCode == 200) {
-            try{
-                var res = JSON.parse(body);
-            }catch(err){
-                return cb(err);
-            }
-            var keys = Object.keys(res);
-            var work = function(i, work_cb){
-                if(i >= keys.length) return work_cb(null);
-                var next_work = function(){
-                    i++;
-                    work(i, work_cb);
-                }
-                var tickerId = keys[i];
-                if(!tickerId) next_work();
-                var ticker = res[tickerId];
-                var at = ticker.at;
-                if(!at) next_work();
-                var buy = +ticker.ticker.buy;
-                if(!buy) next_work();
-                var sell = +ticker.ticker.sell;
-                if(!sell) next_work();
-                
-                CoinPrice.findOne()
-                    .where("tickerId").equals(tickerId)
-                    .exec(function(err, coinPrice){
-                        if(err) return work_cb(err);
-                        if(!coinPrice){
-                            coinPrice = new CoinPrice();
-                            coinPrice.tickerId = tickerId;
-                        }
-                        coinPrice.at = at;
-                        coinPrice.buy = buy;
-                        coinPrice.sell = sell;
-                        coinPrice.save(function(err){
-                            if(err) return cb(err);
-                            next_work();
-                        });
-                    });
-            }
-            work(0, cb);
-        }else{
-            return cb(new Error("status " + response.statusCode));
+    var baseWork = function(try_times){
+        if(try_times > 3) return cb(new Error("try too much"));
+        var retry = function(){
+            try_times++;
+            setTimeout(function(){
+                baseWork(try_times);
+            }, try_times * 200);
         }
-    });
+        request({
+            url: "https://yunbi.com/api/v2/tickers",
+            method: "GET"
+        }, function(err, response, body){
+            if(err) return cb(err);
+            if (response.statusCode == 200) {
+                try{
+                    var res = JSON.parse(body);
+                }catch(err){
+                    return cb(err);
+                }
+                var keys = Object.keys(res);
+                var work = function(i, work_cb){
+                    if(i >= keys.length) return work_cb(null);
+                    var next_work = function(){
+                        i++;
+                        work(i, work_cb);
+                    }
+                    var tickerId = keys[i];
+                    if(!tickerId) next_work();
+                    var ticker = res[tickerId];
+                    var at = ticker.at;
+                    if(!at) next_work();
+                    var buy = +ticker.ticker.buy;
+                    if(!buy) next_work();
+                    var sell = +ticker.ticker.sell;
+                    if(!sell) next_work();
+                    
+                    CoinPrice.findOne()
+                        .where("tickerId").equals(tickerId)
+                        .exec(function(err, coinPrice){
+                            if(err) return work_cb(err);
+                            if(!coinPrice){
+                                coinPrice = new CoinPrice();
+                                coinPrice.tickerId = tickerId;
+                            }
+                            coinPrice.at = at;
+                            coinPrice.buy = buy;
+                            coinPrice.sell = sell;
+                            coinPrice.save(function(err){
+                                if(err) return cb(err);
+                                next_work();
+                            });
+                        });
+                }
+                work(0, cb);
+            }else{
+                return retry();
+            }
+        });
+    }
+    baseWork(0);
 }
 
 coinPriceSchema.plugin(uniqueValidator);
